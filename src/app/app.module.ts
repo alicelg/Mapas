ea41f374aa84a3dd58cdbd61db759ff2333c539f
Alice López Gómez <alicelopezgomez@MacBook-Air-de-Antonio.local>
2020-11-10 14:37:35 +0100

    add HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MapaV2Component } from './mapa-v2/mapa-v2.component';
 
-import { AgmCoreModule } from '@agm/core'
+import { AgmCoreModule } from '@agm/core';
+
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MapaV2Component
+    MapaV2Component,
+
   ],
   imports: [
     BrowserModule,
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyBXoe3vvdGGosbpLVZqUncQDgiW4UAbl58'
-    })
+    }),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
